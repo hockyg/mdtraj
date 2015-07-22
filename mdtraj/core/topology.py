2cc8d0b5a584b7aac92399fc7d618c7145f01a71
Glen Hocky <glen842@midway>
2015-07-22 09:35:38 -0500

    topology_from_subset now preserves segment_id

--- mdtraj/core/topology.py
+++ mdtraj/core/topology.py
@@ -96,6 +96,7 @@ def _topology_from_subset(topology, atom_indices):
                     if not residue == previous_residue:
                         newResidue = newTopology.add_residue(residue.name, newChain,
                                                              resSeq)
+                        newResidue.segment_id = residue.segment_id
                         previous_residue = residue
                     
                     try:  # OpenMM Topology objects don't have serial attributes, so we have to check first.
